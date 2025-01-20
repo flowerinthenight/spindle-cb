93dfe616757627bf7b6317fc254fc89942be99ea
flowerinthenight <root@flowerinthenight.com>
2025-01-20 13:14:16 +0900

    sample asg startup

--- startup-aws-asg.sh
+++ startup-aws-asg.sh
@@ -8,6 +8,33 @@ echo '# Ref: https://github.com/aws/clock-bound/tree/main/clock-bound-d' >> /etc
 echo 'maxclockerror 50' >> /etc/chrony.d/clockbound.conf
 systemctl restart chronyd
 systemctl status chronyd
+cp /clock-bound/target/release/clockbound /usr/local/bin/clockbound
+chown chrony:chrony /usr/local/bin/clockbound
+
+cat >/usr/lib/systemd/system/clockbound.service <<EOL
+[Unit]
+Description=ClockBound
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=10
+ExecStart=/usr/local/bin/clockbound --max-drift-rate 50
+RuntimeDirectory=clockbound
+RuntimeDirectoryPreserve=yes
+WorkingDirectory=/run/clockbound
+User=chrony
+Group=chrony
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+systemctl daemon-reload
+systemctl enable clockbound
+systemctl start clockbound
+systemctl status clockbound
+
 # wget https://github.com/flowerinthenight/zgroup/releases/download/v0.3.2/zgroup-v0.3.2-x86_64-linux.tar.gz
 # tar -xzvf zgroup-v0.3.2-x86_64-linux.tar.gz
 METADATA_TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
