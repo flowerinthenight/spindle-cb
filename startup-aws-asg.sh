8a18e17845f27187df98a20f6dd6c38ad768bd8a
flowerinthenight <root@flowerinthenight.com>
2025-01-28 12:48:30 +0900

    ci skip: update rel ver

--- startup-aws-asg.sh
+++ startup-aws-asg.sh
@@ -41,8 +41,8 @@ cp /clock-bound/clock-bound-ffi/include/clockbound.h /usr/include/
 cp /clock-bound/target/release/libclockbound.a /usr/lib/
 cp /clock-bound/target/release/libclockbound.so /usr/lib/
 
-wget https://github.com/flowerinthenight/spindle-cb/releases/download/v0.1.4/spindle-v0.1.4-x86_64-linux.tar.gz
-tar -xzvf spindle-v0.1.4-x86_64-linux.tar.gz
+wget https://github.com/flowerinthenight/spindle-cb/releases/download/v0.1.5/spindle-v0.1.5-x86_64-linux.tar.gz
+tar -xzvf spindle-v0.1.5-x86_64-linux.tar.gz
 METADATA_TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 INTERNAL_IP=$(curl -H "X-aws-ec2-metadata-token: $METADATA_TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
 echo "internal-ip: $INTERNAL_IP"
